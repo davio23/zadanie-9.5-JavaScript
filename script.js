944ecbf2be51729b9418dd227ae4b2429c45305b
davio23 <33763332+davio23@users.noreply.github.com>
2017-12-04 15:15:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-var button1 = document.getElementsByClassName('button1'),
-    button2 = document.getElementsByClassName('button2'),
-    button3 = document.getElementsByClassName('button3'),
-    button4 = document.getElementsByClassName('button4'),
-    button5 = document.getElementsByClassName('button5');
+var buttons = document.getElementsByClassName('button'),
 for (var i = 0; i < button.length;) {	
-    button1.innerText;
-    button2.innerText;
-    button3.innerText;
-    button4.innetText;
-    button5.innerText;  
-    alert(button1.innerText);
-    alert(button2.innerText);
-    alert(button3.innerText);
-    alert(button4.innerText);
-    alert(button5.innerText);
+    button.innerText; 
+    alert(button.innerText);
 }
